0beddf78f63b5c163a5323f923986595b884381e
Letícia Santos Da Silva <leticiasantos4118@gmail.com>
2025-11-20 21:21:44 -0300

    fazendo o botão funcionar de pesquisa com Java Script

--- base-de-conhecimento/script.js
+++ base-de-conhecimento/script.js
@@ -1,10 +1,24 @@
+let cardContainer = document.querySelector(".card-container");
 let dados = [];
 
 async function iniciarBusca() {
     let resposta = await fetch("data.json");
     dados = await resposta.json();
-    console.log(dados);
-    
+    rederizarCards(dados);
+
 }
 
+function rederizarCards(dados) {
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.ano}</p>
+        <p>${dado.descricao}</p> <!-- O erro estava aqui, a propriedade correta é "descricao" sem o "ç" e o til. -->
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `;
+        cardContainer.appendChild(article);
+    }
 
+}
